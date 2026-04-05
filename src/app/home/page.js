82db628a1bfdfdff9cc7892fa66ebb10158c21e4
ghuleaniketh <ghuleaniketh@gmail.com>
2026-04-05 18:41:00 +0530

    all the rought are sorted and is ready to go for prodaction ----Thank you----

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -69,8 +69,7 @@ export default function VoicePage() {
 
     
 
-    // const response = await fetch("https://moro-backend.onrender.com/chat", {
-    const response = await fetch("http://localhost:8080/chat", {
+    const response = await fetch("https://moro-backend.onrender.com/chat", {
       method: 'POST',
       body: formData
     });
